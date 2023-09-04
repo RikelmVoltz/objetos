0e6e58ce3d6e2c0aa35ebe714e09768bde071b48
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-09-04 17:24:59 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,170 @@
+
+// EXERCICIO1
+/*
+let filme = {
+nome: 'homemAranha2',
+lançamento: 2004,
+direção: 'SamRaimi',
+elenco: ['tobeyMaguire','stanlee','alfredMolina'],
+assistido: true
+}
+
+console.log(filme.nome);
+console.log(filme.lançamento);
+console.log(filme.direção);
+console.log(filme.elenco);
+console.log(filme.assistido);
+
+EXERCICIO2
+
+let pessoa = {
+    nome: 'Rikelme Voltz',
+    idade: 16,
+    mscPreferida: 'pop',
+
+}
+console.log(" O nome dessa pessoa é " , pessoa.nome + " ela tem " ,  pessoa.idade  +  " anos e gosta muito de ", pessoa.mscPreferida);
+
+let filme = {
+    nome: 'homemAranha2',
+    lançamento: 2004,
+    direção: 'SamRaimi',
+    elenco: ['tobeyMaguire','Willem Dafoe','alfredMolina'],
+    assistido: true,
+    }
+    filme.personagens = ['homemAranha','duendeVerde','doutorOctopus'],
+
+    
+    console.log(filme.elenco[0] +" fez"+ filme.personagens[0]);
+    console.log(filme.elenco[1] +" fez "+ filme.personagens[1]);
+    console.log(filme.elenco[2] +" fez "+ filme.personagens[2]);
+
+    filme.elenco[0] = 'xuxa';
+    console.log(filme.elenco[0]);
+    console.log(filme);
+
+const usuario = {
+    nome: 'prof',
+    idade: 25,
+    email: 'prof@senac.com.br',
+    cidade: 'são paulo'
+ }
+
+const novoUsuario = {
+    ...usuario,
+    nome: 'joão',
+    idade: 28
+}
+
+console.log(usuario);
+console.log(novoUsuario);
+
+
+const listaDeNomes = ["Mika", "Paula" , "vitor"]
+const copialistaDenomes = [...listaDeNomes]
+
+console.log(copialistaDenomes)
+
+
+// EXERCICIO4
+
+let pessoa = {
+    nome: 'Rikelme Voltz',
+    idade: 16,
+    mscPreferida: 'pop',
+
+
+}
+
+function lerObjetoPessoas(objeto){
+    let novoObjeto = {...objeto}
+    novoObjeto.ComidasPreferidas = ['comida1', 'comida2', 'comida3'];
+    novoObjeto.
+}
+
+*/
+
+// EXERCICIO1
+/*
+let filme = {
+nome: 'homemAranha2',
+lançamento: 2004,
+direção: 'SamRaimi',
+elenco: ['tobeyMaguire','stanlee','alfredMolina'],
+assistido: true
+}
+
+console.log(filme.nome);
+console.log(filme.lançamento);
+console.log(filme.direção);
+console.log(filme.elenco);
+console.log(filme.assistido);
+
+EXERCICIO2
+
+let pessoa = {
+    nome: 'Rikelme Voltz',
+    idade: 16,
+    mscPreferida: 'pop',
+
+}
+console.log(" O nome dessa pessoa é " , pessoa.nome + " ela tem " ,  pessoa.idade  +  " anos e gosta muito de ", pessoa.mscPreferida);
+
+let filme = {
+    nome: 'homemAranha2',
+    lançamento: 2004,
+    direção: 'SamRaimi',
+    elenco: ['tobeyMaguire','Willem Dafoe','alfredMolina'],
+    assistido: true,
+    }
+    filme.personagens = ['homemAranha','duendeVerde','doutorOctopus'],
+
+    
+    console.log(filme.elenco[0] +" fez"+ filme.personagens[0]);
+    console.log(filme.elenco[1] +" fez "+ filme.personagens[1]);
+    console.log(filme.elenco[2] +" fez "+ filme.personagens[2]);
+
+    filme.elenco[0] = 'xuxa';
+    console.log(filme.elenco[0]);
+    console.log(filme);
+
+const usuario = {
+    nome: 'prof',
+    idade: 25,
+    email: 'prof@senac.com.br',
+    cidade: 'são paulo'
+ }
+
+const novoUsuario = {
+    ...usuario,
+    nome: 'joão',
+    idade: 28
+}
+
+console.log(usuario);
+console.log(novoUsuario);
+
+
+const listaDeNomes = ["Mika", "Paula" , "vitor"]
+const copialistaDenomes = [...listaDeNomes]
+
+console.log(copialistaDenomes)
+
+
+// EXERCICIO4
+
+let pessoa = {
+    nome: 'Rikelme Voltz',
+    idade: 16,
+    mscPreferida: 'pop',
+
+
+}
+
+function lerObjetoPessoas(objeto){
+    let novoObjeto = {...objeto}
+    novoObjeto.ComidasPreferidas = ['comida1', 'comida2', 'comida3'];
+    novoObjeto.
+}
+
+*/
